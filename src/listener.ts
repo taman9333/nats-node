0cd3c246b9bfb9893d85e18d69c5fa7d97deb62f
taman9333 <taman9333@gmail.com>
2020-09-05 14:03:41 +0200

    Add clientId & queue group for scaling listener & make sure events only sent to 1 instance inside queue group

--- src/listener.ts
+++ src/listener.ts
@@ -1,16 +1,20 @@
 import nats, { Message } from 'node-nats-streaming';
+import { randomBytes } from 'crypto';
 
 console.clear();
 
 //client
-const stan = nats.connect('ticketing', '123', {
+const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
   url: 'http://localhost:4222'
 });
 
 stan.on('connect', () => {
   console.log('Listener connected to nats');
 
-  const subscription = stan.subscribe('ticket:created');
+  const subscription = stan.subscribe(
+    'ticket:created',
+    'orders-service-queue-group'
+  );
 
   subscription.on('message', (msg: Message) => {
     const data = msg.getData();
