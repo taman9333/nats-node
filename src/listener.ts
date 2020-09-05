1034004ec854e882dd94c531f731c41b32615ef3
taman9333 <taman9333@gmail.com>
2020-09-05 16:13:05 +0200

    Enable graceful client shutdown instead of waiting to close connection

--- src/listener.ts
+++ src/listener.ts
@@ -11,6 +11,11 @@ const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
 stan.on('connect', () => {
   console.log('Listener connected to nats');
 
+  stan.on('close', () => {
+    console.log('NATS connection closed!');
+    process.exit();
+  });
+
   const options = stan.subscriptionOptions().setManualAckMode(true);
   const subscription = stan.subscribe(
     'ticket:created',
@@ -28,3 +33,6 @@ stan.on('connect', () => {
     msg.ack();
   });
 });
+
+process.on('SIGINT', () => stan.close());
+process.on('SIGTERM', () => stan.close());
